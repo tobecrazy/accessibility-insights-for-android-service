b01d608b773a038ce891bfc2b03be18de790e24d
Young Liu <young.liu@sap.com>
2020-08-10 17:16:24 +0800

    add ignore

--- AccessibilityInsightsForAndroidService/app/src/main/java/com/gui/MainActivity.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/gui/MainActivity.java
@@ -1,10 +1,12 @@
 package com.gui;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +16,7 @@ import com.microsoft.accessibilityinsightsforandroidservice.R;
 public class MainActivity extends AppCompatActivity {
     Button button;
 
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

50d15139c9b311255913710bad400b1584b5d0cf
Young Liu <young.liu@sap.com>
2020-09-12 18:36:06 +0800

    add setting button to enable Accessibility

--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/gui/MainActivity.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/gui/MainActivity.java
@@ -6,23 +6,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.microsoft.accessibilityinsightsforandroidservice.R;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button copyButton;
+    Button settingButton;
 
     @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.click_to_copy);
-        button.setOnClickListener(v -> {
+        copyButton = findViewById(R.id.click_to_copy);
+        copyButton.setOnClickListener(v -> {
                     ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                     ClipData clip = ClipData.newPlainText(getText(R.string.label), getText(R.string.command));
                     assert cm != null;
@@ -30,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, getText(R.string.command), Toast.LENGTH_LONG).show();
                 }
         );
-
+        settingButton = findViewById(R.id.click_to_setting);
+        settingButton.setOnClickListener(v -> {
+            Intent settings = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(settings);
+        });
 
     }
 }

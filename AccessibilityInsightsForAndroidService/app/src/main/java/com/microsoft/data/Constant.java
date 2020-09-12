84237ef864ac296bda9aeaee6708b0b067ca48cc
Young Liu <young.liu@sap.com>
2020-09-12 18:10:39 +0800

    downgrade gradle target sdk to resolve crash issuee

---
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/data/Constant.java
@@ -0,0 +1,9 @@
+package com.microsoft.data;
+
+/**
+ * Create by i321533 (young.liu@sap.com) on 09/12/2020
+ **/
+public class Constant {
+    public static final String DATA = "data";
+    public static final String RESULT_CODE = "code";
+}
--- AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/gui/ScreenshotActivity.java
+++ AccessibilityInsightsForAndroidService/app/src/main/java/com/microsoft/gui/ScreenshotActivity.java
@@ -6,37 +6,51 @@ package com.microsoft.gui;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.projection.MediaProjectionManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
+import com.microsoft.accessibilityinsightsforandroidservice.AccessibilityInsightsForAndroidService;
 import com.microsoft.accessibilityinsightsforandroidservice.MediaProjectionHolder;
 import com.microsoft.accessibilityinsightsforandroidservice.R;
+import com.microsoft.data.Constant;
 
 public class ScreenshotActivity extends Activity {
-  private MediaProjectionManager mediaManager;
-  private static final int SCREENSHOT = 99999;
+    private MediaProjectionManager mediaManager;
+    private static final int SCREENSHOT = 99999;
 
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    mediaManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
-    startActivityForResult(mediaManager.createScreenCaptureIntent(), SCREENSHOT);
-  }
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mediaManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+        if (mediaManager != null) {
+            startActivityForResult(mediaManager.createScreenCaptureIntent(), SCREENSHOT);
+        } else {
+            showErrorMessage(getString(R.string.screenshot_permission_not_granted));
+        }
 
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == SCREENSHOT) {
-      if (resultCode == RESULT_OK) {
-        MediaProjectionHolder.set(mediaManager.getMediaProjection(resultCode, data));
-      }
     }
 
-    if (MediaProjectionHolder.get() == null) {
-      Toast.makeText(this, R.string.screenshot_permission_not_granted, Toast.LENGTH_LONG).show();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SCREENSHOT) {
+            if (resultCode == RESULT_OK) {
+                MediaProjectionHolder.set(mediaManager.getMediaProjection(resultCode, data));
+                if (MediaProjectionHolder.get() == null) {
+                    showErrorMessage(getString(R.string.screenshot_permission_not_granted));
+                }
+            }
+        }
+        finish();
     }
 
-    finish();
-  }
+    /**
+     * @param message
+     */
+    private void showErrorMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
 }
